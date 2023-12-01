cdac352999ffdd067ada08c7de62de3809bb161e
Kahsolt <kahsolt@qq.com>
2023-12-01 12:03:43 +0800

    use the same feats

--- src/models/ccqc.py
+++ src/models/ccqc.py
@@ -3,29 +3,10 @@
 # Create Time: 2023/10/31
 
 from src.models import ModelMSE
+from src.features import feats
 from src.utils import *
 
 # implements the ansatz in "Circuit-centric quantum classifiers": https://arxiv.org/abs/1804.00633
-
-feats = [
-  'Title',            # [0, 4]
-  'cnt(Name_lst)',    # [1, 6]
-  'cnt(Name_fst)',    # [1, 13]
-  'Name_ex',          # [0, 1]
-  'bin(Age)',         # [0, 4]
-  'cat(Sex)',         # [0, 1]
-  'Parch',            # [0, 6]
-  'Family_add',       # [0, 10]
-  'Family_sub-min',   # [0, 11]
-  'Family_sub_abs',   # [0, 6]
-  #'Family_mul',       # [0, 16]
-  'Pclass-1',         # [0, 2]
-  'bin(Fare)',        # [0, 5]
-  'log(Fare)',        # [1.3894144, 4.849553]
-  'bin(log(Fare))',   # [0, 4]
-  'cat(Cabin_pf)',    # [0, 8]
-  'cat(Ticket_pf)',   # [0, 11]
-]
 
 
 class CU3(Module):
